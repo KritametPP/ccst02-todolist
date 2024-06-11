3ab0ff5d22bbbde0fdebed5b0aa0548abe04c02f
KritametPP <kritamet.p@gmail.com>
2024-06-11 15:22:56 +0700

    add Delete Todolist

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,26 @@
+import axios from 'axios'
 import React from 'react'
 
-function TodoItem({job}) {
+function TodoItem({job, reload}) {
+
+  const hdlDelete = async () => {
+    if(!confirm('Confirm to delete ?')){
+      return
+    }
+    try {
+      await axios.delete(`http://localhost:8000/jobs/${job.id}`)
+      reload()
+    } catch (error) {
+      console.log(error.message)
+    }
+    
+  }
+
   return (
     <div className="todo-item">
       <input type="text" disabled value={job.todo}/>
       <button>Edit</button>
-      <button>Delete</button>
+      <button onClick={hdlDelete}>Delete</button>
     </div>
   )
 }
