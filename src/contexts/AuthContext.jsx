3c6d44f0603acc4870a94b3a7b774e593aaf7f09
KritametPP <kritamet.p@gmail.com>
2024-06-11 11:15:02 +0700

    Router and Nav for user/guest

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,14 +1,18 @@
-import React, { createContext, useState } from 'react'
+import {createContext, useState} from 'react'
 
 const AuthContext = createContext()
+
 const userLogin = localStorage.getItem('user')
 
-function AuthContextProvider(props) {
-  const [isLogin, setIsLogin] = useState(!!userLogin)
-  const [user, setUser] = useState(userLogin ? JSON.parse(userLogin) : null)
+
+
+function AuthContextProvider({children}) {
+  const [user, setUser] = useState(JSON.parse(userLogin))
+  const isLogin = () => !!user
+
   return (
-    <AuthContext.Provider value={{ isLogin, setIsLogin, user, setUser }}>
-      {props.children}
+    <AuthContext.Provider value={{user, setUser}}>
+      {children}
     </AuthContext.Provider>
   )
 }
