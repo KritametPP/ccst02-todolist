be3a46cef90d96e1b6fa356d5f6de4fef75a7567
KritametPP <kritamet.p@gmail.com>
2024-06-11 11:47:12 +0700

    TodoApp ui mock UI : Dashboard

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from "../components/TodoItem";
+
+function TodoContainer() {
+  return (
+    <div>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+    </div>
+  )
+}
+
+export default TodoContainer
